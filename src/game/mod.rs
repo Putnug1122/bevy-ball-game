43f350031c901450629ef1e5569516efbdfaf6a4
Putra Nugraha <putnug1122@gmail.com>
2023-07-13 21:12:25 +0800

    add early state but not complete

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -4,6 +4,7 @@ pub mod enemy;
 mod player;
 pub mod score;
 pub mod star;
+mod systems;
 
 use enemy::EnemyPlugin;
 use player::PlayerPlugin;
@@ -11,15 +12,26 @@ use score::ScorePlugin;
 use star::StarPlugin;
 
 use crate::events::GameOver;
+use crate::game::systems::toggle_simulation;
+use crate::AppState;
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_event::<GameOver>()
+        app.add_state::<SimulationState>()
+            .add_event::<GameOver>()
             .add_plugin(PlayerPlugin)
             .add_plugin(EnemyPlugin)
             .add_plugin(StarPlugin)
-            .add_plugin(ScorePlugin);
+            .add_plugin(ScorePlugin)
+            .add_system(toggle_simulation.run_if(in_state(AppState::Game)));
     }
+}
+
+#[derive(States, Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+pub enum SimulationState {
+    Running,
+    #[default]
+    Paused,
 }
--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,21 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(GamePlugin)
+        .add_state::<AppState>()
         .add_plugin(MainMenuPlugin)
+        .add_plugin(GamePlugin)
         .add_startup_system(spawn_camera)
+        .add_system(transition_to_game_state)
+        .add_system(transition_to_main_menu_state)
         .add_system(exit_game)
         .add_system(handle_game_over)
         .run()
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    Game,
+    GameOver,
 }
